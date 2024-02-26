167bd74fa25096bc8e3acdd997d53b78ea5a4079
Aditya Surana <aditya.surana@proshore.eu>
2024-02-26 15:13:43 +0545

    stripe webhook created for payment notification

--- payment/views.py
+++ payment/views.py
@@ -7,18 +7,18 @@ from orders.models import Order
 
 # Create your views here.
 stripe.api_key = settings.STRIPE_SECRET_KEY
-stripe.api_version = settings.STRIPT_API_VERSION
+stripe.api_version = settings.STRIPE_API_VERSION
 
 def payment_process(request):
     order_id = request.session.get('order_id', None)
     order = get_object_or_404(Order, id=order_id)
     if request.method == 'POST':
         success_url = request.build_absolute_uri(reverse('payment:completed'))
-        cancel_url = request.build_absoulute_uri(reverse('payment:canceled'))
+        cancel_url = request.build_absolute_uri(reverse('payment:canceled'))
         session_data = {
             'mode': 'payment',
-            'clent_reference_id': order.id,
-            'succcess_url': success_url,
+            'client_reference_id': order.id,
+            'success_url': success_url,
             'cancel_url': cancel_url,
             'line_items': []
         }
@@ -40,7 +40,7 @@ def payment_process(request):
         return render(request, 'payment/process.html', locals())
     
 def payment_completed(request):
-    return render(request, 'payment/compleated.html')
+    return render(request, 'payment/completed.html')
 
 def payment_canceled(request):
     return render(request, 'payment/canceled.html')
