ef0260a98c872773e9dbd55532ac5f5daf42f05e
Aditya Surana <aditya.surana@proshore.eu>
2024-02-29 17:03:51 +0545

    Export to csv functinality added to admin page

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Order, OrderItem
 from django.utils.safestring import mark_safe
+from django.http import HttpResponse
 
 # Register your models here.
 
@@ -16,10 +17,28 @@ def order_payment(obj):
     return ''
 order_payment.short_description = 'Stripe Payment'
 
+def download_csv(modeladmin, request, queryset):
+    import csv
+    opts = queryset.model._meta
+    response = HttpResponse(content_type="text/csv")
+    response['Content-Disposition'] = 'attachment;filename=export.csv'
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display =  ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', order_payment, 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [download_csv]
 
